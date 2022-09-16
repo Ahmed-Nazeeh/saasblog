cf394a464a541b329ad7e68e4d8c1ada0ed9ebc5
Ahmed-Nazeeh <aa.nazeeh@gmail.com>
2022-09-16 14:42:17 +0200

    add stripe webhooks

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :posts
   post "checkout/create", to: "checkout#create", as: "checkout_create"
   post "billing_portal/create", to: "billing_portal#create", as: "billing_portal_create"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :webhooks, only: [:create]
 end
